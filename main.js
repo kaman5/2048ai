54fbd396f4675cb1bb3dc971d43fa98628877ac3
kaman5 <94335616+kaman5@users.noreply.github.com>
2023-01-01 21:40:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+
+// grab html tiles
+var board = [];
+
+var tiles = Array.prototype.slice.call(document.getElementsByClassName("tile"));
+while(tiles.length) board.push(tiles.splice(0,5));
+board.reverse();
+
+var b = new Board(board);
+var engine = new Engine(b);
+b.addRandomTile();
+b.addRandomTile();
+document.addEventListener('keydown', keyListen, false);
+
+
+
+
+
+mainLoop();
+var limFrame = 0;
+var delay = 50;
+
+function mainLoop() {
+    limFrame++;
+
+    if(limFrame % delay == 0) {
+        let box = document.getElementById("computer-play").checked;
+
+        if(!box) {
+            document.addEventListener('keydown', keyListen, false);
+        }
+        else {
+            document.removeEventListener('keydown', keyListen, false);
+            engine.play();
+        }
+    }
+    requestAnimationFrame(mainLoop);
+}
+
+function keyListen(event) {
+    var name = event.key;
+    var code = event.code;
+
+    if(code === "ArrowRight") {
+        b.shiftRight();
+    }
+    else if(code === "ArrowUp") {
+        b.shiftUp();
+    }
+    else if(code === "ArrowDown") {
+        b.shiftDown();
+    }
+    else if(code === "ArrowLeft") {
+        b.shiftLeft();
+    }
+
+    if(code === "ArrowRight" || code === "ArrowUp" || code === "ArrowDown" || code === "ArrowLeft") {
+        b.addRandomTile();
+        engine.updateBar();
+        //engine.showLines(b.getPosition());
+    }
+}
+
+
+
+
+
+
